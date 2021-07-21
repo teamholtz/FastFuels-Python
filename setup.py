3d48e42a15ba22eb899c02e8a97fc9451cd3809b
Daniel Crawl <danielcrawl@gmail.com>
2021-07-21 16:01:10 -0700

    adding shapely and bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 setup(
   name = 'fastfuels',
   packages = ['fastfuels'],
-  version = '0.5.1',
+  version = '0.5.2',
   license='GNU GPLv3',
   description = '3D fuelscapes for the contiguous US',
   long_description = README,
@@ -25,6 +25,7 @@ setup(
           'pyvista',
           's3fs',
           'scipy',
+          'shapely',
           'zarr',
       ],
   classifiers=[
