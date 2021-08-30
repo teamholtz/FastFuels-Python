381cd2c338ce82fdf7a6433e9bbe739d789b90d7
lucaswells <lucas@holtzforestry.com>
2021-08-30 16:35:09 -0600

    release version 0.5.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 setup(
   name = 'fastfuels',
   packages = ['fastfuels'],
-  version = '0.5.4',
+  version = '0.5.5',
   license='GNU GPLv3',
   description = '3D fuelscapes for the contiguous US',
   long_description = README,
